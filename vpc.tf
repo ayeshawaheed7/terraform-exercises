76e090cae87c442e6fbf2a83ef1c153c69d93f40
ayeshawaheed7 <ayeshawaheed451@gmail.com>
2025-06-18 10:29:21 +0500

    make vpc configurable for diff envs

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,5 @@
 terraform {
-    backend "s3" {
-      bucket = "my-app-tfstatebucket"
-      key = "my-app/state.tfstate"
-      region = "ap-southeast-1"
-    }
+    backend "s3" {}
 }
 
 provider "aws" {
@@ -12,19 +8,14 @@ provider "aws" {
 
 data "aws_availability_zones" "azs" {}
 
-resource "random_string" "suffix" {
-    length = 8
-    special = false
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.18.1"
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  name = "${var.env_prefix}-my-vpc"
+  cidr = var.vpc_cidr_block
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets = var.public_subnet_cidr_blocks
   azs = data.aws_availability_zones.azs.names
 
   enable_nat_gateway = true
