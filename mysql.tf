fc54f20e543b60acc857d0f8c19587d231d3cfb8
ayeshawaheed7 <ayeshawaheed451@gmail.com>
2025-06-18 14:18:30 +0500

    Add MySQL Helm release using Bitnami chart with EKS provider setup

--- mysql.tf
+++ mysql.tf
@@ -13,3 +13,28 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+resource "helm_release" "mysql" {
+  name       = "mysql"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "mysql"
+  version    = "9.1.4 "
+
+  values     = [
+    file("mysql-values.yaml")
+  ]
+
+  set {
+    name  = "volumePermissions.enabled"
+    value = true
+  }
+}
